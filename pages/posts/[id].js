0876333687b8e384cc20ec1bbfd5c7864171f3cf
Erin Ponsonby <erinponsonby@gmail.com>
2022-01-07 13:52:18 -0500

    Format date

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,11 +1,14 @@
 // the filename must include [] in order for route to be dynamic
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
 // getPostData function is being used here (in getStaticProps)
 // to get the post data and return it as props
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
@@ -25,11 +28,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
     return (
       <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+        <article>
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
       </Layout>
     )
   }
